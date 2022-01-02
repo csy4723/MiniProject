cde7ecc9201a6a96db6b9edcf7b4a04550ffb89c
seo young choi <csy4723@gmail.com>
2022-01-02 09:31:15 +0900

    캠핑장 등록이랑 공지사항

--- src/com/uni/miniProject/controller/CampingController.java
+++ src/com/uni/miniProject/controller/CampingController.java
@@ -1,5 +1,173 @@
 package com.uni.miniProject.controller;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
+
+import com.uni.miniProject.model.vo.CampInfo;
+
 public class CampingController {
+	ArrayList<CampInfo> camp = new ArrayList<CampInfo>();// 캠핑 예약 상품 담을 리스트
+	Scanner sc = new Scanner(System.in);
+	CampInfo cinfo = new CampInfo();
+	FileOutputStream fos = null;
+
+	public CampingController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void campRegister() {
+
+		System.out.println("캠핑장 이름 : ");
+		String Name = sc.nextLine();
+
+		System.out.println("캠핑장 지역 : ");
+		String Area = sc.nextLine();
+
+		System.out.println("가격 : ");
+		int price = sc.nextInt();
+		sc.nextLine();
+
+		System.out.println("이용 날짜(yyyy-MM-dd) : ");
+		String date = sc.nextLine();
+		String[] dArr = date.split("-");
+
+		int year = Integer.valueOf(dArr[0]);
+		int month = Integer.valueOf(dArr[1]);
+		int day = Integer.valueOf(dArr[2]);
+
+		Calendar cDate = Calendar.getInstance();
+		cDate.set(year, month, day);
+
+		cinfo = new CampInfo(Name, Area, price, cDate);
+
+		camp.add(cinfo);// 여기까지 ArrayList에 캠핑목록 담는 거
+
+	}
+
+	public void campList() {
+
+		for(int i = 0; i<camp.size(); i++) {
+			System.out.println(camp.get(i).information());
+		}
+		
+	}
+
+	public void campDelete() {
+
+		System.out.println("삭제할 캠핑장 이름을 입력하세요");
+		String name = sc.nextLine();
+
+		for (int i = 0; i < camp.size(); i++) {
+
+			if (name.equals(camp.get(i).getCampName())) {
+				camp.remove(i);
+				return;
+			}
+
+		}
+		System.out.println("삭제할 캠핑장이 없습니다.");
+	}
+
+	public void campRead() {// 파일에 있던 객체를 ArrayList에 담아주는 메소드
+
+		File f = new File("camping List.txt");
+		ArrayList tem = null;
+
+		try (BufferedReader br = new BufferedReader(new FileReader(f.getName()))) {
+
+			String temp = null;
+			while ((temp = br.readLine()) != null) {
+				
+			
+
+				String[] arr = temp.split(",");
+				
+
+				String name = arr[0];
+				String area = arr[1];
+				int price = Integer.valueOf(arr[2]);
+
+				String[] arr2 = arr[3].split("-");
+
+				int year = Integer.valueOf(arr2[0]);
+				int month = Integer.valueOf(arr2[1]);
+				int day = Integer.valueOf(arr2[2]);
+
+				CampInfo c = new CampInfo(name, area, price, setCalendar(year, month, day));
+				
+				camp.add(c);
+
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(EOFException e) {
+			
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+		
+	}
+	
+	
+	
+	public void campWrite() {
+		
+		File folder = new File("C:\\Camping List");
+		
+		if(!folder.exists()) {
+			folder.mkdir();
+		}
+		
+		File ex = new File(folder, "Camping List.txt");
+		
+		if(ex.exists()) {
+			ex.delete();
+		}
+		
+		
+		File f = new File("camping List.txt");
+		
+	try(BufferedWriter bw = new BufferedWriter(new FileWriter(f.getName()))){
+		
+		for(int i = 0; i < camp.size(); i++) {
+			
+			bw.write(camp.get(i).toString());
+			bw.newLine();
+			
+		}
+		
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+		
+	}
+
+	private Calendar setCalendar(int year, int month, int day) {
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(year, month, day);
+
+		return cal;
+	}
 
 }
--- src/com/uni/miniProject/view/MainMenu.java
+++ src/com/uni/miniProject/view/MainMenu.java
@@ -1,38 +1,148 @@
 package com.uni.miniProject.view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.uni.miniProject.controller.CampingController;
+import com.uni.miniProject.controller.MemberController;
+import com.uni.miniProject.model.vo.Write;
 
 public class MainMenu {
-	
+	public static String ID;
+
+	MemberController mc = new MemberController();
 	CampingController cc = new CampingController();
 	Scanner sc = new Scanner(System.in);
-	
+
 	public MainMenu() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public void mainMenu() {
+		cc.campRead(); // run에서 이걸 먼저 실행하고 main 실행하면 camp이 자꾸 초기화 된다 왜인지 모를 
 		
-		System.out.println("1. 로그인 하시겠습니까? ");
-		System.out.println("2. 회원 가입 하시겠습니가? ");
-		System.out.print("메뉴 입력 : ");
-		int openMenu = sc.nextInt();
-		
-		
-		
-		System.out.println("====메인 메뉴===");
-		System.out.println("1. 마이 페이지");
-		System.out.println("2. 캠핑 예약");
-		System.out.println("3. 양도게시판");
-		System.out.println("4. 자유게시판");
-		System.out.println("5. 리뷰게시판");
-		System.out.println("9. 프로그램 종료");
-		System.out.print("메뉴 입력 : ");
-		int menu = sc.nextInt();
-		
+		while (true) {
+			System.out.println("1. 로그인 하시겠습니까? ");
+			System.out.println("2. 회원 가입 하시겠습니가? ");
+			System.out.print("메뉴 입력 : ");
+			int openMenu = sc.nextInt();
+			sc.nextLine();
+
+			boolean out = false;// 해당 while문 나가기 위한 장치 -서영
+
+			switch (openMenu) {
+			case 1:
+				ID = "admin";// 유저가 아이디 입력한 값을 ID에 담아주세요
+				out = true;
+				break;
+			case 2:
+				out = true;
+				break;
+			default:
+				System.out.println("잘못 입력하셨습니다.다시 입력하세요");
+				continue;
+			}
+			if (out) {
+				break; // out이 true면 while문 나감
+			}
+		}
+
+		while (true) {
+
+			System.out.println("====메인 메뉴===");
+
+			if (ID.equals("admin")) {
+				System.out.println("0. 관리자 페이지");
+			}
+			System.out.println("1. 마이 페이지");
+			System.out.println("2. 캠핑 예약");
+			System.out.println("3. 양도게시판");
+			System.out.println("4. 자유게시판");
+			System.out.println("5. 리뷰게시판");
+			System.out.println("9. 프로그램 종료");
+			System.out.print("메뉴 입력 : ");
+			int menu = sc.nextInt();
+			sc.nextLine();
+
+			switch (menu) {
+			case 0:
+				adminPage();
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 9:
+				cc.campWrite();
+				System.out.println("프로그램이 종료됩니다.");
+				return;
+			default:
+				System.out.println("잘못 입력했습니다. 다시 입력하세요");
+				break;
+
+			}
+
+		}
 	}
-	
+
+	private void adminPage() {
+		while (true) {
+
+			System.out.println("===관리자 페이지===");
+			System.out.println("1. 전체 글 조회");
+			System.out.println("2. 공지사항 조회");
+			System.out.println("3. 캠핑장 목록 조회");
+			System.out.println("9. 이전 메뉴로");
+			int adminMenu = sc.nextInt();
+			sc.nextLine();
+
+			switch (adminMenu) {
+			case 1:
+				break;
+			case 2:
+				mc.noticList();
+				System.out.println("1. 공지 사항을 등록하시겠습니까? ");
+				System.out.println("2. 공지사항을 삭제하시겠습니까? ");
+				int nMenu = sc.nextInt();
+				sc.nextLine();
+
+				if (nMenu == 1) {
+					mc.postNotice();
+				} else if (nMenu == 2) {
+					mc.deleteNotice();
+				}
+
+				break;
+			case 3:
+				cc.campList();
+				System.out.println("1. 캠핑장을 등록하시겠습니까? ");
+				System.out.println("2. 캠핑장을 삭제하시겠습니까? ");
+				int cMenu = sc.nextInt();
+				sc.nextLine();
+
+				if (cMenu == 1) {
+					cc.campRegister();
+				} else if (cMenu == 2) {
+					cc.campDelete();
+				}
+
+				break;
+			case 9:
+				return;
+			default:
+				System.out.println("잘못 입력했습니다.");
+				break;
+
+			}
+
+		}
+
+	}
 
 }
