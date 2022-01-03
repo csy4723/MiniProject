220bbfecdb397b5e5d1ff6054badb5d51f11a34d
chkm306 <chkm306@gmail.com>
2022-01-04 03:18:36 +0900

    캠핑리스트 오류, 리뷰

--- src/com/uni/miniProject/controller/ReviewController.java
+++ src/com/uni/miniProject/controller/ReviewController.java
@@ -10,13 +10,13 @@ public class ReviewController {
 	}
 
 	public void reviewSearch() {
-		System.out.println("===리뷰 게시글 검색===");
 		System.out.println("[등록된 리뷰 목록]");
 		
-		
-		System.out.println("검색할 게시글 입력 : ");
-		String keyword = sc.nextLine();
+		System.out.println("===리뷰 게시글 검색===");		
+		System.out.println("검색할 제목 입력 : ");
+		String head = sc.nextLine();
 
+		
 		System.out.println("===검색 결과===");
 		
 		/*for(int i = 0; i < camp.size(); i++) {
@@ -24,6 +24,8 @@ public class ReviewController {
 				System.out.println(camp.get(i).information());
 			}
 		}*/
+		System.out.println("계속 검색하시겠습니까?(y/n)(n을 누르면 돌아갑니다) : ");
+		char ch = sc.nextLine().toUpperCase().charAt(0);
 	}
 	
 	public void reviewSort() {
